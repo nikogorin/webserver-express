f4795c6eb50ceec20151c518a13ba3597d30ab30
Nicolas Piccolini <nicolas.piccolini@engee.com.ar>
2020-03-02 02:46:03 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const puerto = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        name: 'nicolas'
+    });
+})
+app.get('/about', (req, res) => {
+
+    res.render('about');
+})
+app.listen(puerto, () => {
+    console.log(`Escuchando peticiones en el puerto ${puerto}`);
+});
